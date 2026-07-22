9e30cffe59e7f8414f74dfae3f754f2857043718
Oxidaner <18622412361@163.com>
2026-07-22 10:38:45 +0800

    fix(router): preserve external error origin
    Signed-off-by: Oxidaner <18622412361@163.com>

--- pkg/kthena-router/router/router.go
+++ pkg/kthena-router/router/router.go
@@ -451,7 +451,6 @@ func (r *Router) doLoadbalance(c *gin.Context, modelRequest ModelRequest) error 
 					if proxyErr.origin != "" {
 						accesslog.SetErrorOrigin(c, proxyErr.origin)
 					}
-					accesslog.SetErrorOrigin(c, "router")
 					c.Set("finishReason", proxyErr.reason)
 					if !c.Writer.Written() {
 						c.AbortWithStatusJSON(proxyErr.statusCode, proxyErr.message)

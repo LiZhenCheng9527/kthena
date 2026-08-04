0e9f29402ea2b44059f374595d06dc0502d19360
Avinash Kumar Deepak <avinash8655279@gmail.com>
2026-08-04 13:15:26 +0530

    model-serving: track grace periods by pod UID
    Signed-off-by: Avinash Kumar Deepak <avinash8655279@gmail.com>

--- pkg/model-serving-controller/controller/model_serving_controller_grace_test.go
+++ pkg/model-serving-controller/controller/model_serving_controller_grace_test.go
@@ -19,10 +19,12 @@ package controller
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	workloadv1alpha1 "github.com/volcano-sh/kthena/pkg/apis/workload/v1alpha1"
+	"github.com/volcano-sh/kthena/pkg/model-serving-controller/datastore"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -31,6 +33,7 @@ import (
 	corelisters "k8s.io/client-go/listers/core/v1"
 	kubetesting "k8s.io/client-go/testing"
 	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
 )
 
 func TestHandlePodAfterGraceTime(t *testing.T) {
@@ -143,6 +146,62 @@ func TestHandlePodWithoutGraceTimeUsesUIDPrecondition(t *testing.T) {
 	require.NotNil(t, deleteAction.GetDeleteOptions().Preconditions)
 	require.NotNil(t, deleteAction.GetDeleteOptions().Preconditions.UID)
 	assert.Equal(t, podUID, *deleteAction.GetDeleteOptions().Preconditions.UID)
+}
+
+func TestHandleErrorPodTracksReplacementByUID(t *testing.T) {
+	const (
+		namespace = "default"
+		podName   = "test-model-0-prefill-0-0"
+	)
+	gracePeriod := int64(1)
+	failedPod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: namespace,
+			Name:      podName,
+			UID:       types.UID("failed-pod"),
+		},
+	}
+	replacementPod := failedPod.DeepCopy()
+	replacementPod.UID = types.UID("replacement-pod")
+	replacementPod.Status.Phase = corev1.PodPending
+
+	ms := &workloadv1alpha1.ModelServing{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: namespace,
+			Name:      "test-model",
+		},
+		Spec: workloadv1alpha1.ModelServingSpec{
+			Template: workloadv1alpha1.ServingGroup{
+				RestartGracePeriodSeconds: &gracePeriod,
+			},
+		},
+	}
+	controller, kubeClient := newGracePeriodTestController(t, replacementPod)
+	controller.store = datastore.New()
+	controller.workqueue = workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter()) //nolint:staticcheck
+	t.Cleanup(controller.workqueue.ShutDown)
+
+	require.NoError(t, controller.handleErrorPod(ms, "test-model-0", failedPod))
+	require.NoError(t, controller.handleErrorPod(ms, "test-model-0", replacementPod))
+
+	_, failedPodTracked := controller.graceMap.Load(getPodGracePeriodKey(failedPod))
+	_, replacementPodTracked := controller.graceMap.Load(getPodGracePeriodKey(replacementPod))
+	require.True(t, failedPodTracked)
+	require.True(t, replacementPodTracked)
+
+	require.Eventually(t, func() bool {
+		for _, action := range kubeClient.Actions() {
+			deleteAction, ok := action.(kubetesting.DeleteAction)
+			if !ok || !action.Matches("delete", "pods") {
+				continue
+			}
+			preconditions := deleteAction.GetDeleteOptions().Preconditions
+			if preconditions != nil && preconditions.UID != nil && *preconditions.UID == replacementPod.UID {
+				return true
+			}
+		}
+		return false
+	}, 2*time.Second, 10*time.Millisecond)
 }
 
 func newGracePeriodTestController(t *testing.T, pod *corev1.Pod) (*ModelServingController, *kubefake.Clientset) {
